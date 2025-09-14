c5ee1caed3d2d71f109ef1fde31389a92ab6123d
Linda5-umwali <l.umwali@alustudent.com>
2025-09-14 14:30:45 -0700

    basemodel init modification

--- models/base_model.py
+++ models/base_model.py
@@ -11,10 +11,20 @@ class BaseModel:
     """Defines all common attributes/methods for other classes"""
 
     def __init__(self, *args, **kwargs):
-        """Initialize a new BaseModel"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        """Initialize a new BaseModel instance"""
+        if kwargs and len(kwargs) != 0:
+            for key, value in kwargs.items():
+                if key == "__class__":
+                    continue
+                if key in ("created_at", "updated_at"):
+                    # Convert string to datetime
+                    setattr(self, key, datetime.fromisoformat(value))
+                else:
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def save(self):
         """Updates updated_at with current datetime"""
